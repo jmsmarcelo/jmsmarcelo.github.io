f00183366598551b4ea9a081d83d9dab3e4f92fb
jmsmarcelo <jmsmarcelos@gmail.com>
2025-08-15 23:21:21 -0400

    fix loading repositories without languages

--- assets/scripts/container-main-1.js
+++ assets/scripts/container-main-1.js
@@ -21,10 +21,8 @@ function loadRepo() {
                 elems[repo.name + '_title'] = [repo.name + '_link', 'h3', [['textContent', repo.name], ['classList', 'repo-title']]];
                 elems[repo.name + '_desc'] = [repo.name + '_link', 'p', [['innerHTML', repo.description || '&nbsp;'], ['classList', 'repo-desc']]];
                 elems[repo.name + '_info'] = [repo.name + '_link', 'div', [['classList', 'repo-info']]];
-                elems[repo.name + '_lang'] = [repo.name + '_info', 'span', [['innerHTML', repo.language || '&nbsp;'], ['classList', `repo-lang ${repo.language.toLowerCase().replace(/\+/g, 'p')}`]]];
-                if(repo.stargazers_count > 0) {
-                    elems[repo.name + '_star'] = [repo.name + '_info', 'span', [['innerHTML', repo.stargazers_count || '&nbsp;'], ['classList', 'repo-icons star']]];
-                }
+                if(repo.language) elems[repo.name + '_lang'] = [repo.name + '_info', 'span', [['innerHTML', repo.language || '&nbsp;'], ['classList', `repo-lang ${repo.language.toLowerCase().replace(/\+/g, 'p')}`]]];
+                if(repo.stargazers_count > 0) elems[repo.name + '_star'] = [repo.name + '_info', 'span', [['innerHTML', repo.stargazers_count || '&nbsp;'], ['classList', 'repo-icons star']]];
             });
             createElems(elems, {repos: document.querySelector('.repos')}, containerMainLang[html.lang][1]);
             Object.assign(containerMain[1], elems);
