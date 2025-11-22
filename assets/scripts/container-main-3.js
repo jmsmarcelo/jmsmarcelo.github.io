d7077b80df6fd359f8bc60ad377518d82f6c29de
Jose Marcelo <32857346+jmsmarcelo@users.noreply.github.com>
2025-11-21 20:36:16 -0400

    Update email contact in container-main-3.js

--- assets/scripts/container-main-3.js
+++ assets/scripts/container-main-3.js
@@ -2,6 +2,6 @@ containerMain[3] = {
     'contacts': ['main', 'fieldset', [['classList', 'contacts']]],
     'title': ['contacts', 'legend', [['textContent', 'lang__title']]],
     'text': ['contacts', 'p', [['textContent', 'lang__text']]],
-    'linkedin': ['contacts', 'a', [['classList', 'icon-sm email'], ['href', 'mailto: jmsmarcelo@gmail.com'], ['target', '_blank']]],
+    'email': ['contacts', 'a', [['classList', 'icon-sm email'], ['href', 'mailto: jmsmarcelos@gmail.com'], ['target', '_blank']]],
     'wa': ['contacts', 'a', [['classList', 'icon-sm wa'], ['href', 'https://wa.me/5569992879084'], ['target', '_blank']]]
 };
